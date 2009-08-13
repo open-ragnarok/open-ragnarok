fbfa28aaa35c8312ceeafad3c760ca1a94923772
Sergio Moura <sergio@moura.us>
2009-08-13 18:45:38 +0000

    Reverting unwanted changes

--- ronet/packets/pkt_maplogin.cc
+++ ronet/packets/pkt_maplogin.cc
@@ -3,21 +3,32 @@
 
 #include "ronet/packets/pkt_maplogin.h"
 
-ronet::pktMapLogin::pktMapLogin() : Packet(pktMapLoginID) {
+ronet::pktMapLogin::pktMapLogin() :
+	Packet(pktMapLoginID) {
+}
+
+ronet::pktMapLogin::pktMapLogin(unsigned int id1, unsigned int id2,
+		unsigned int id3, unsigned int tick, char s) :
+	Packet(pktMapLoginID) {
+	session_id1 = id1;
+	session_id2 = id2;
+	session_id3 = id3;
+	this->tick = tick;
+	gender = s;
 }
 
 bool ronet::pktMapLogin::Decode(ucBuffer& buf) {
 	unsigned short buf_id;
-	buf.peek((unsigned char*)&buf_id, 2);
+	buf.peek((unsigned char*) &buf_id, 2);
 	if (buf_id != id) {
 		fprintf(stderr, "Wrong packet id! (%04x != %04x)\n", id, buf_id);
-		return(false);
+		return (false);
 	}
 	unsigned short size;
-	size = *(unsigned short*)(buf.getBuffer() + 2);
+	size = *(unsigned short*) (buf.getBuffer() + 2);
 
 	if (buf.dataSize() < 3) // Not enough data
-		return(false);
+		return (false);
 	buf.ignore(2);
 	buf >> session_id1;
 	buf >> session_id2;
@@ -25,5 +36,25 @@ bool ronet::pktMapLogin::Decode(ucBuffer& buf) {
 	buf >> tick;
 	buf >> gender;
 
-	return(true);
+	return (true);
+}
+
+void ronet::pktMapLogin::setGender(char gender) {
+	this->gender = gender;
+}
+
+void ronet::pktMapLogin::setSession_id1(unsigned int session_id1) {
+	this->session_id1 = session_id1;
+}
+
+void ronet::pktMapLogin::setSession_id2(unsigned int session_id2) {
+	this->session_id2 = session_id2;
+}
+
+void ronet::pktMapLogin::setSession_id3(unsigned int session_id3) {
+	this->session_id3 = session_id3;
+}
+
+void ronet::pktMapLogin::setTick(unsigned int tick) {
+	this->tick = tick;
 }
