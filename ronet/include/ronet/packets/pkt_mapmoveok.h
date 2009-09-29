2225b5a0b46eb859a3a8fbeaaa7b7b989fa5dc8a
Sergio Moura <sergio@moura.us>
2009-09-29 19:05:51 +0000

    Header / License update Removed unused files

--- ronet/include/ronet/packets/pkt_mapmoveok.h
+++ ronet/include/ronet/packets/pkt_mapmoveok.h
@@ -1,9 +1,36 @@
 /* $Id$ */
+/*
+    ------------------------------------------------------------------------------------
+    LICENSE:
+    ------------------------------------------------------------------------------------
+    This file is part of The Open Ragnarok Project
+    Copyright 2007 - 2009 The Open Ragnarok Team
+    For the latest information visit http://www.open-ragnarok.org
+    ------------------------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with
+    this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+    Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+    http://www.gnu.org/copyleft/lesser.txt.
+    ------------------------------------------------------------------------------------
+*/
 #ifndef __RONET_PACKET_MAPMOVEOK_H
 #define __RONET_PACKET_MAPMOVEOK_H
 
 #include "ronet/packet.h"
 
+/*
+0000 | 87 00 45 1f c7 05 0d c7 0 0 ec 70 88             | ..E..... ..p.
+---- | _ID__ _Ticks_____ _Pos___ _Dest__ xx             | -------- ----
+*/
 namespace ronet {
 	class RONET_DLLAPI pktMapMoveOk : public Packet {
 	protected:
@@ -17,6 +44,7 @@ namespace ronet {
 
 		void getStart(int*, int*) const;
 		void getDest(int*, int*) const;
+		int getTicks() const;
 	};
 }
 
--- ronet/packets/pkt_mapmoveok.cc
+++ ronet/packets/pkt_mapmoveok.cc
@@ -1,3 +1,27 @@
+/* $Id$ */
+/*
+    ------------------------------------------------------------------------------------
+    LICENSE:
+    ------------------------------------------------------------------------------------
+    This file is part of The Open Ragnarok Project
+    Copyright 2007 - 2009 The Open Ragnarok Team
+    For the latest information visit http://www.open-ragnarok.org
+    ------------------------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with
+    this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+    Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+    http://www.gnu.org/copyleft/lesser.txt.
+    ------------------------------------------------------------------------------------
+*/
 #include "stdafx.h"
 
 #include "ronet/packets/pkt_maploaded.h"
@@ -10,6 +34,9 @@ pktMapMoveOk::pktMapMoveOk() : Packet(pktMapMoveOkID) {
 	dest_x = dest_y = 0;
 }
 
+int pktMapMoveOk::getTicks() const {
+	return(ticks);
+}
 
 bool pktMapMoveOk::Decode(ucBuffer& buf) {
 	unsigned short buf_id;
@@ -25,13 +52,10 @@ bool pktMapMoveOk::Decode(ucBuffer& buf) {
 	if (buf.dataSize() < 12)
 		return(false);
 
-	buf.ignore(2);
-
-	unsigned char coord[5];
-
-
+	buf.ignore(2); // id
 	buf >> ticks;
 	
+	unsigned char coord[5];
 	buf.read(coord, 5);
 	unsigned char *ptr = (unsigned char*)&v;
 	v = 0;
